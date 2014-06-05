51da98dc7c860336bbcd4d6413ecc43636fd5396
Jeremy Abbott <jeremymabbott@gmail.com>
2014-06-05 13:10:25 -0500

    Initial Commit

---
+++ src/FeedReader/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace FeedReader.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/FeedReader/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FeedReader.Startup))]
+namespace FeedReader
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
